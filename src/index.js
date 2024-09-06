d28465a349ea1a822cf2069450ead4f2c72d4f14
isaaccrvg <isaaccrvg98@hotmail.com>
2024-09-06 11:23:49 -0300

    Grid adicionado

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Hello from './Hello';
+import Grid from './Grid';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Hello />
+    <Grid/>
   </React.StrictMode>
 );
 
